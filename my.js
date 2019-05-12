edb68d016e3b2aa92cae2c1d30cb0b74e3bf5ecc
akashsachithanandham <46894142+akashsachithanandham@users.noreply.github.com>
2019-05-12 16:37:48 +0530

    Create my.js

---
+++ my.js
@@ -0,0 +1,7 @@
+function clicked(){
+    
+    if(document.getElementById("names").style.display =="block")
+    document.getElementById("names").style.display ="none";
+    else 
+    document.getElementById("names").style.display ="block";
+}
